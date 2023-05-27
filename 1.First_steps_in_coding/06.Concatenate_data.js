e8934ffe7272e61a2f47f1561744f1c01f455f2b
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-05-27 18:02:45 +0300

    Create 06.Concatenate_data.js

---
+++ 1.First_steps_in_coding/06.Concatenate_data.js
@@ -0,0 +1,10 @@
+function concatenate(input) {
+    let firstName = input[0];
+    let lastName = input[1];
+    let age = input[2];
+    let town = input[3];
+    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`)
+
+}
+
+concatenate(["Sofia", "Ivanova", "22", "Sofia"])
