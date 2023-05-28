e5aa3b31a68810f43a6d843988a7cc19aa6d7200
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-05-28 22:32:02 +0300

    Create 2.Greater_number.js

---
+++ 2.Conditional_statements/2.Greater_number.js
@@ -0,0 +1,12 @@
+function isGreater(input) {
+    let number1 = Number(input[0])
+    let number2 = Number(input[1])
+    if (number1 >= number2) {
+        console.log(number1);
+    }
+    else {
+        console.log(number2);
+    }
+}
+
+isGreater(["5", "3"])
