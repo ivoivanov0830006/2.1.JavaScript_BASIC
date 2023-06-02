eeb2737898eb3dd3f47b07d6afb8544a7235b46c
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-02 15:11:36 +0300

    Update 11.Fruit_shop.js

--- 3.Conditional_statements_advanced/11.Fruit_shop.js
+++ 3.Conditional_statements_advanced/11.Fruit_shop.js
@@ -1,1 +1,38 @@
+function shop(input) {
+    let product = String(input[0]);
+    let day = String(input[1]);
+    let quantity = Number(input[2]);
+    let price = 0;
+    let validProduct = true;
+    let validDay = true;
 
+    // Workdays prices 
+    if (day === "Monday" || 
+        day === "Tuesday" || 
+        day == "Wednesday" || 
+        day === "Thursday" || 
+        day === "Friday") {
+        if (product === "banana") {
+            price = 2.5;
+        }
+        else if (product === "apple") {
+            price = 1.2;
+        }
+        else if (product === "orange") {
+            price = 0.85;
+        }
+        else if (product === "grapefruit") {
+            price = 1.45;
+        }
+        else if (product === "kiwi") {
+            price = 2.7;
+        }
+        else if (product === "pineapple") {
+            price = 5.5;
+        }
+        else if (product === "grapes") {
+            price = 3.85;
+        } 
+        else {
+            validProduct = false
+        }
