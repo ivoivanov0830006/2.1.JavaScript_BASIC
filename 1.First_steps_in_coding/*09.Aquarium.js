2921f3d3735afc51ab6bdeb1bb9b26eb9dd8c511
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-04 22:57:44 +0300

    Create *09.Aquarium.js

---
+++ 1.First_steps_in_coding/*09.Aquarium.js
@@ -0,0 +1,18 @@
+function calculate(input) {
+    // Input Data:
+    let length = Number(input[0]);
+    let width = Number(input[1]);
+    let height = Number(input[2]);
+    let percentage = Number(input[3]);
+
+    // Volume Calculation:
+    let aquariumVolume = length * width * height
+
+    // Total Cubic Liters Calculation
+    let totalLiters = (aquariumVolume - aquariumVolume * (percentage / 100)) / 1000
+
+    // Total Expenses
+    console.log(totalLiters);
+}
+
+calculate(["85 ","75 ","47 ","17 "])
