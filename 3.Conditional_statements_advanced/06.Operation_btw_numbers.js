3456150753828e97fed4f884ef9fc1db63aa9d2f
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-06 22:38:12 +0300

    Create 06.Operation_btw_numbers.js

---
+++ 3.Conditional_statements_advanced/06.Operation_btw_numbers.js
@@ -0,0 +1,44 @@
+function mathOpertation(input) {
+    num1 = Number(input[0]);
+    num2 = Number(input[1]);
+    operator = input[2];
+
+    if (num2 == 0) {
+        console.log(`Cannot divide ${num1} by zero`)
+    } else {
+        if (operator === "+") {
+            let result = num1 + num2;
+            if (result % 2 == 0) {
+                console.log(`${num1} ${operator} ${num2} = ${result} - even`)
+            } else {
+                console.log(`${num1} ${operator} ${num2} = ${result} - odd`)
+            }
+        }
+        else if (operator === "-") {
+            let result = num1 - num2;
+            if (result % 2 == 0) {
+                console.log(`${num1} ${operator} ${num2} = ${result} - even`)
+            } else {
+                console.log(`${num1} ${operator} ${num2} = ${result} - odd`)
+            }
+        }
+        else if (operator === "*") {
+            let result = num1 * num2;
+            if (result % 2 == 0) {
+                console.log(`${num1} ${operator} ${num2} = ${result} - even`)
+            } else {
+                console.log(`${num1} ${operator} ${num2} = ${result} - odd`)
+            }
+        }
+        else if (operator === "/") {
+            let result = num1 / num2;
+            console.log(`${num1} ${operator} ${num2} = ${result.toFixed(2)}`)
+            }
+        else if (operator === "%") {
+            let result = num1 % num2;
+            console.log(`${num1} ${operator} ${num2} = ${result}`)
+        }
+    } 
+}
+
+mathOpertation(["10","12","+"])
