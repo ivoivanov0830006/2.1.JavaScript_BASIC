83e3a9b65c22f9befb70564a322b05a72de2ddf8
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-07 20:06:10 +0300

    Update 06.Sum_vowels.js

--- 4.For_loops/06.Sum_vowels.js
+++ 4.For_loops/06.Sum_vowels.js
@@ -18,3 +18,24 @@ function vowels(input) {
 }
 
 vowels(["hello"])
+
+
+// --------------------------- Another Solution ------------------------------
+
+// function vowels(input) {
+//     let text = input[0];
+//     let total = 0
+//     for (let i = 0; i < text.length; i++) {
+//         let char = text[i]
+//         switch(char) {
+//             case "a": total += 1; break; 
+//             case "e": total += 2; break;
+//             case "i": total += 3; break;
+//             case "o": total += 4; break;
+//             case "u": total += 5; break;
+//         }
+//     }
+//     console.log(total)
+// }
+
+// vowels(["hello"]) 
