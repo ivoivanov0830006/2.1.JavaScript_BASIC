09589db7b2f1300610b7899f7044d5048598ea9b
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-09 22:54:04 +0300

    Create 02.Password.js

---
+++ 5.While_loops/02.Password.js
@@ -0,0 +1,12 @@
+function password(input) {
+    let username = input[0];
+    let pass = input[1];
+    let data = input[2];
+    let index = 3;
+    
+    while (data !== pass) {
+        data = input[index];
+        index++;
+    }
+    console.log(`Welcome ${username}!`)
+}
