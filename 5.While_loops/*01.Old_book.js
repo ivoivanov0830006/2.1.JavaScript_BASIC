924b9796fe72ddc8e2e4ba1a9e7055508dad732c
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-10 23:13:47 +0300

    Create *01.Old_book.js

---
+++ 5.While_loops/*01.Old_book.js
@@ -0,0 +1,25 @@
+function books(input) {
+    let searchedBook = input[0];
+    let currentBook = input[1];
+    let index = 2;
+    let isFound = false;
+    let checkedBooks = 0;
+
+    while(index <=input.length) {
+        if (currentBook === searchedBook) {
+            isFound = true;
+            break;
+        } else if (currentBook === "No More Books") {
+            break;
+        } else {
+            checkedBooks += 1
+            currentBook = input[index];
+            index++;
+        }
+    }
+    if (isFound) {
+        console.log(`You checked ${checkedBooks} books and found it.`)
+    } else {
+        console.log(`The book you search is not here!\nYou checked ${checkedBooks} books.`)
+    }
+}
