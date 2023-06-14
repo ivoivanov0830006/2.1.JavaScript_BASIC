746be4b4379aeb9ee41b77e798ddeff6e3bd48ff
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-14 22:27:44 +0300

    Create 02.Multiplication_table.js

---
+++ 6.Nested_loops/02.Multiplication_table.js
@@ -0,0 +1,10 @@
+function table() {
+    for (let num1 = 1; num1 <= 10; num1++) {
+        for (let num2 = 1; num2 <= 10; num2++) {
+            let result = num1 * num2
+            console.log(`${num1} * ${num2} = ${result}`)
+        }
+    }
+}
+
+table()
