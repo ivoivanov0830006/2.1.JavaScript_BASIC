d54ed6a1e183bce382a4d682e6ae749336b2a1fd
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-15 18:20:09 +0300

    Create 03.Combinations.js

---
+++ 6.Nested_loops/03.Combinations.js
@@ -0,0 +1,17 @@
+function combinations(input) {
+    let totalCombinations = 0;
+    let n = Number(input[0]);
+
+    for (let x1 = 0; x1 <= n; x1++) {
+        for (let x2 = 0; x2 <= n; x2++) {
+            for (let x3 = 0; x3 <= n; x3++) {
+                if (x1 + x2 + x3 === n) {
+                    totalCombinations += 1;
+                }
+            }
+        }
+    }
+    console.log(totalCombinations)
+}
+
+combinations(["25"])
