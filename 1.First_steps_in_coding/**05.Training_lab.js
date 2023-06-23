ead87b14649d3fa2d18932859668c9f50350e406
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-23 22:32:42 +0300

    Create **05.Training_lab.js

---
+++ 1.First_steps_in_coding/**05.Training_lab.js
@@ -0,0 +1,17 @@
+function trainingHall(input) {
+    // Input Data
+    let width = Number(input[0]);
+    let height = Number(input[1]);
+
+    let deskWidth = 1.2;
+    let deskHeight = 0.7;
+    let corridor = 1;
+
+    let columns = Math.floor((height - corridor) / deskHeight);
+    let rows = Math.floor(width / deskWidth);
+
+    let count = (columns * rows - 3)
+    console.log(count)
+}
+
+trainingHall(["8.5","5.2"])
