7abbc728441b78aca7d1d6ef4859ba2703fa3864
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-06-29 23:07:24 +0300

    Update **01.Match_tickets.js

--- 3.Conditional_statements_advanced/**01.Match_tickets.js
+++ 3.Conditional_statements_advanced/**01.Match_tickets.js
@@ -23,7 +23,6 @@ function tickets(input) {
         discount = 0.4;
     } else {
         discount = 0.25;
-    }
 
     let transportCost = budget * discount;
     let currentMoney = budget - transportCost;
