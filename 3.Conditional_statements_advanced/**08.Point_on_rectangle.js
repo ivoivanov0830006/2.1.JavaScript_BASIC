617566841626021e1505526d69b9169019d777ba
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-07-01 21:53:25 +0300

    Create **08.Point_on_rectangle.js

---
+++ 3.Conditional_statements_advanced/**08.Point_on_rectangle.js
@@ -0,0 +1,25 @@
+function coordinates(input) {
+    let x1 = Number(input[0]);
+    let y1 = Number(input[1]);
+    let x2 = Number(input[2]);
+    let y2 = Number(input[3]);
+    let x = Number(input[4]);
+    let y = Number(input[5]);
+
+    if (((x === x1) && (y >= y1 && y <= y2)) ||
+        ((x === x2) && (y >= y1 && y <= y2)) || 
+        ((y === y1) && (x >= x1 && x <= x2)) ||
+        ((y === y2) && (x >= x1 && x <= x2))) {
+        console.log("Border")
+    } else {
+        console.log("Inside / Outside")
+    }
+}
+
+coordinates (["2","-3",
+            "12","3",
+            "8","-1"])
+
+coordinates (["2", "-3",
+            "12", "3", 
+            "12", "-1"])
