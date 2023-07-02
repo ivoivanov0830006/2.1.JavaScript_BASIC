754987b4ce28772bf739c5b12e3cd6230d9f2178
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-07-02 22:24:46 +0300

    Create **05.Challenge_the_wedding.js

---
+++ 6.Nested_loops/**05.Challenge_the_wedding.js
@@ -0,0 +1,25 @@
+ function wedding(input) {
+    let men = Number(input[0]);
+    let women = Number(input[1]);
+    let tables = Number(input[2]);
+
+    let result = "";
+    let tableCount = 0;
+
+    let noTables = false;
+
+    for (let i = 1; i <= men; i++) {
+        for (let j = 1; j <= women; j++) {
+            tableCount += 1
+            result += `(${i} <-> ${j}) `;
+            if (tableCount === tables) {
+                noTables = true;
+                break;
+            }
+        }   
+        if (noTables) {
+            break;
+        }
+    }
+    console.log(result)
+}
