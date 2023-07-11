627809e579b4adaba46a913148413c2a182fad69
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-07-11 23:02:30 +0300

    Create 3.Aluminum_joinery.js

---
+++ 0.Exams/3.Aluminum_joinery.js
@@ -0,0 +1,15 @@
+function joinery(input) {
+    let count = Number(input[0]);
+    let type = input[1];
+    let delivery = input[2];
+
+    let deliveryCost = 60;
+
+
+    let total = count * price;
+    console.log(`${total} BGN`)
+}
+
+joinery(["40", 
+    "90X130",
+    "Without delivery"])
